738e9078515e5748d1f509ede868af19d1ba1820
junni01kim <audwnssp@naver.com>
2024-12-04 22:08:03 +0900

    SignUpRepository 오타 수정

--- src/main/java/com/project/api/signup/SignUpRepository.java
+++ src/main/java/com/project/api/signup/SignUpRepository.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 import java.util.List;
 
 @Repository
-public interface SignUpRepository extends JpaRepository<Restaurant, Long> {
+public interface SignUpRepository extends JpaRepository<SignUp, Long> {
     Optional<SignUp> findBySignUpId(Long signUpId);
 
     Optional<List<SignUp>> findByUser(User user);
