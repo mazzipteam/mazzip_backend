25aee03af68e2c30228d469c89a1aa4bd383eb54
junni01kim <audwnssp@naver.com>
2024-12-05 13:13:12 +0900

    유저아이디로 노티스 조회하는 기능 추가

--- src/main/java/com/project/api/notice/NoticeService.java
+++ src/main/java/com/project/api/notice/NoticeService.java
@@ -1,10 +1,13 @@
 package com.project.api.notice;
 
+import com.project.api.bookmark.BookmarkService;
 import com.project.api.user.UserService;
 import com.project.entity.Notice;
 import com.project.exception.ControlledException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 import java.util.List;
 
 import static com.project.exception.error_code.NoticeErrorCode.NOTICE_NOT_FOUND;
@@ -15,6 +18,7 @@ import static com.project.exception.error_code.NoticeErrorCode.NOTICE_NOT_FOUND_
 public class NoticeService {
     private final UserService userService;
     private final NoticeRepository noticeRepository;
+    private final BookmarkService bookmarkService;
 
     public Notice create(NoticeCreateDTO noticeCreateDTO) {
         var notice = Notice.builder()
@@ -41,10 +45,14 @@ public class NoticeService {
     }
 
     public List<Notice> getAllNotice(Long userId) {
-        var user = userService.getUser(userId);
+        var bookmarks = bookmarkService.getAllBookmarkByUser(userId);
 
-        var notices = noticeRepository.findByUser(user)
-                .orElseThrow(()-> new ControlledException(NOTICE_NOT_FOUND_BY_USER));
+        var notices = new ArrayList<Notice>();
+        for (var bookmark : bookmarks) {
+            var noticeList = noticeRepository.findByUser(bookmark.getUser())
+                    .orElseThrow(()-> new ControlledException(NOTICE_NOT_FOUND));
+            notices.addAll(noticeList);
+        }
 
         return notices;
     }
